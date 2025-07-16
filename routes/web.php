ee7594be9d0d2081fc462b2111a390fb79537b30
ama hory <horyzontalhoror@gmail.com>
2025-07-16 21:08:22 +0700

    add env

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,8 @@ Route::get('/login', fn () => redirect('/admin/login'))->name('login');
 Route::get('/tes-auth', function () {
     return dd(Auth::guard('web')->user());
 });
+
+Route::middleware('auth:sanctum')->post('/logout', function (Request $request) {
+    $request->user()->currentAccessToken()->delete();
+    return response()->json(['message' => 'Logout berhasil']);
+});
